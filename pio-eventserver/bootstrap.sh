2092f88df8fe560fe87a06f678fcb55d72d0fdf5
Nguyen Viet Anh <vietanh.nguyen.it@gmail.com>
2016-06-22 11:02:37 -0700

    bump version to 0.9.6 and wait until zookeper fully started

--- pio-eventserver/bootstrap.sh
+++ pio-eventserver/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 env.sh
-# wait.sh $HBASE_HOST:$ZOOKEEPER_PORT
-echo "sleeping $SLEEP seconds ..." 
+wait.sh $HBASE_MASTER_IP:$ZOOKEEPER_PORT 
+wait.sh $ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT
+echo "Sleeping another $SLEEP seconds to make sure the Zookeeper fully started..." 
 sleep $SLEEP
-pio eventserver
+pio eventserver 
